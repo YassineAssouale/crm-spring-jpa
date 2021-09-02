3be54c5199bdc73bf7d7dc529f517795c046dc47
Yassine <yassine.assouale@gmail.com>
2021-09-02 11:08:02 +0200

    Adding MyOrderRepository - TO DELETE

--- src/main/java/crm/jpa/repository/MyOrderRepository.java
+++ src/main/java/crm/jpa/repository/MyOrderRepository.java
@@ -3,6 +3,7 @@ package crm.jpa.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import crm.jpa.model.Order;
